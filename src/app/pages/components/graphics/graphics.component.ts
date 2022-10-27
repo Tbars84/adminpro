75fbbdf7d3054ec8a86d57cf9f5835b6a551428f
Camilo Tabares <camoilo.tabares.84@gmail.com>
2022-10-26 23:39:42 -0500

    Donut component working as expected

--- src/app/pages/components/graphics/graphics.component.ts
+++ src/app/pages/components/graphics/graphics.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ChartData, ChartEvent, ChartType } from 'chart.js';
+
 
 @Component({
   selector: 'app-graphics',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraphicsComponent implements OnInit {
 
-  constructor() { }
+  @Input() donutData!: any;
+  // Doughnut
+  public doughnutChartLabels!: string[];
+  public doughnutChartData!: ChartData<'doughnut'>;
+  ngOnInit() {
+    this.doughnutChartLabels= this.donutData.labelsGraphic;
+    this.doughnutChartData= {
+      labels: this.doughnutChartLabels,
+      datasets: [
+        { data: this.donutData.data }
+      ]
+    };
+  }
+  public doughnutChartType: ChartType = 'doughnut';
 
-  ngOnInit(): void {
+  // events
+  public chartClicked({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    console.log(event, active);
   }
 
+  public chartHovered({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
 }
--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -6,7 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./grafica1.component.css']
 })
 export class Grafica1Component implements OnInit {
-
+  donutsData = [
+    {
+      title: 'Sales',
+      labelsGraphic: ['Download sales', 'In-store sales' , 'Mail-order sales'],
+      data: [ 350, 400, 100 ]
+    },
+    {
+      title: 'Incomings',
+      labelsGraphic: ['Download incomings', 'In-store incomings' , 'Mail-order incomings'],
+      data: [ 600, 200, 150 ]
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
